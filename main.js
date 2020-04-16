c06a8f2eeb6c12cfbddfae7b3eef7e5206278807
Stephen Dodd <skyledodd1989@gmail.com>
2020-04-15 21:58:24 -0500

    everything should be set, but getting 403 error

--- main.js
+++ main.js
@@ -1,76 +1,104 @@
-//'X-Api-Key'=NUKvX2jbb8rNB6KVyzdqGObtrWRRDaY5rnEscNA6
+const APIkey = {
+    headers: new Headers({
+        "x-api-key":'IVqMcW16jeMCCyCdfkRdd9KfjrZMhJYAwOpSdjB4',
+        "accept": 'application/json'})};
+
 const stateAbbrev = {
-    alabama: 'al',
-    alaska: 'ak',
-    arizona: 'az',
-    arkansas: 'ar',
-    california: 'ca',
-    colorado: 'co',
-    connecticut: 'ct',
-    delaware: 'de',
-    florida: 'fl',
-    georgia: 'ga',
-    hawaii: 'hi',
-    idaho: 'id',
-    illinois: 'il',
-    indiana: 'in',
-    iowa: 'ia',
-    kansas: 'ks',
-    kentucky: 'ky',
-    louisiana: 'la',
-    maine: 'me',
-    maryland: 'md',
-    massachusetts: 'ma',
-    michigan: 'mi',
-    minnesota: 'mn',
-    mississippi: 'ms',
-    missouri: 'mo',
-    montana: 'mt',
-    nebraska: 'ne',
-    nevada: 'nv',
-    'new hampshire': 'nh',
-    'new jersey': 'nj',
-    'new mexico': 'nm',
-    'new york': 'ny',
-    'north carolina': 'nc',
-    'north dakota': 'nd',
-    ohio: 'oh',
-    oklahoma: 'ok',
-    oregon: 'or',
-    pennsylvania: 'pa',
-    'rhode island': 'ri',
-    'south carolina': 'sc',
-    'south dakota': 'sd',
-    tennessee: 'tn',
-    texas: 'tx',
-    utah: 'ut',
-    vermont: 'vt',
-    virginia: 'va',
-    washington: 'wa',
-    'west virginia': 'wv',
-    wisconsin: 'wi',
-    wyoming: 'wy'
+    ALABAMA: 'AL',
+    ALASKA: 'AK',
+    ARIZONA: 'AZ',
+    ARKANSAS: 'AR',
+    CALIFORNIA: 'CA',
+    COLORADO: 'CO',
+    CONNECTICUT: 'CT',
+    DELAWARE: 'DE',
+    FLORIDA: 'FL',
+    GEORGIA: 'GA',
+    HAWAII: 'HI',
+    IDAHO: 'ID',
+    ILLINOIS: 'IL',
+    INDIANA: 'IN',
+    IOWA: 'IA',
+    KANSAS: 'KS',
+    KENTUCKY: 'KY',
+    LOUISIANA: 'LA',
+    MAINE: 'ME',
+    MARYLAND: 'MD',
+    MASSACHUSSETTS: 'MA',
+    MICHIGAN: 'MI',
+    MINNESOTA: 'MN',
+    MISSISSIPPI: 'MS',
+    MISSOURI: 'MO',
+    MONTANA: 'MT',
+    NEBRASKA: 'NE',
+    NEVADA: 'NV',
+    NEWHAMPSHIRE: 'NH',
+    NEWJERSEY: 'NJ',
+    NEWMEXICO: 'NM',
+    NEWYORK: 'NY',
+    NORTHCAROLINA: 'NC',
+    NORTHDAKOTA: 'ND',
+    OHIO: 'OH',
+    OKLAHOMA: 'OR',
+    OREGON: 'OR',
+    PENNSYLVANIA: 'PA',
+    RHODEISLAND: 'RI',
+    SOUTHCAROLINA: 'SC',
+    SOUTHDAKOTA: 'SD',
+    TENNESSEE: 'TN',
+    TEXAS: 'TX',
+    UTAH: 'UT',
+    VERMONT: 'VT',
+    VIRGINIA: 'VA',
+    WASHINGTON: 'WA',
+    WESTVIRGINIA: 'WV',
+    WISCONSIN: 'WI',
+    WYOMING: 'WY'
 }
 
+function displayResults(responseJson) {
+    console.log(responseJson);
+}
 
-function formatQuery(userInput, numResults) {
+function getParksResponse(url, options) {
+    fetch(url,options).then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    })
+}
+
+function formatUrl(userInputString, maxResults) {
+    let getNationalParks = `https://developer.nps.gov/api/vl/parks?${userInputString}&limit=${maxResults}`;
+    getParksResponse(getNationalParks, APIkey)
+}
+
+function formatUserInput(userInput, numResults) {
     console.log(userInput);
+    let userInputArray = [];
     for (let i = 0; i < userInput.length; i++) {
         if (stateAbbrev[userInput[i]] !== undefined) {
-            console.log(`${stateAbbrev[userInput[i]]}`)
+            userInputArray.push(`stateCode=${stateAbbrev[userInput[i]]}`)
         }
         else if (Object.values(stateAbbrev).indexOf(userInput[i]) !== -1){
-            console.log(`${userInput[i]}`);
+            userInputArray.push(`stateCode=${userInput[i]}`);
         }
     }
+    userInputString = userInputArray.join('&')
+    formatUrl(userInputString, numResults);
 }
 
 function findUserInput() {
     $('form').submit(event => {
         event.preventDefault();
-        let userInput = $('#userStates').val().toLowerCase().replace(/\s/g,'').split(',');
+        let userInput = $('#userStates').val().toUpperCase().replace(/\s/g,'').split(',');
         let numResults = $('#maxResults').val();
-        formatQuery(userInput, numResults)
+        formatUserInput(userInput, numResults)
     })
 }
 
